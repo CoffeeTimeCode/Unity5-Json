3f290e051843a9a3ec4579ff60e9c8b33cb88058
jsnunes <jesiel.nunes@hotmail.com>
2017-06-04 19:10:44 -0300

    Lendo um Json

--- Assets/C#/UnityJson.cs
+++ Assets/C#/UnityJson.cs
@@ -7,10 +7,17 @@ public class UnityJson : MonoBehaviour {
 	public string _json;
 
 	void Start () {
+		//Criando um json
 		_game = new Game("Teste","Rpg");
 		_json = JsonUtility.ToJson(_game);
 		print (_json);
 		//{"nome":"Teste","categoria":"Rpg","lancamento":"1/10/2017 6:04:44 PM"}
+
+		//Lendo um json
+		_game = JsonUtility.FromJson<Game>("{\"nome\":\"Teste\",\"categoria\":\"Rpg\",\"lancamento\":\"1/10/2017 6:04:44 PM\"}");
+		print (_game.nome);
+		print (_game.categoria);
+		print (_game.lancamento);
 	}	
 
 	void Update () {}
--- Assets/JavaScript/UnityJson.js
+++ Assets/JavaScript/UnityJson.js
@@ -3,9 +3,17 @@ public var _game: Game;
 public var _json: String;
 
 function Start () {
+	//Criando um json
 	_game = new Game("Teste","Rpg");
 	_json = JsonUtility.ToJson(_game);
 	print(_json);
+	
+	//Lendo um json
+	_game = JsonUtility.FromJson("{\"nome\":\"Teste\",\"categoria\":\"Rpg\",\"lancamento\":\"1/10/2017 6:04:44 PM\"}",Game);
+	print (_game.nome);
+	print (_game.categoria);
+	print (_game.lancamento);
+	
 }
 
 function Update () {}
